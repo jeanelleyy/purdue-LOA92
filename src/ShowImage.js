d3575ad378c0ef0fbfc35317f4664a4193c51d41
jeanelleyy <jeanelle.t@gmail.com>
2021-10-17 16:06:24 -0700

    initial

---
+++ src/ShowImage.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+class ShowImage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.switchImage = this.switchImage.bind(this);
+    this.state = {
+      currentImage: 0,
+      seconds: 0
+    };
+  }
+
+  switchImage() {
+    if (this.state.currentImage == 0) {
+      this.setState({
+        currentImage: 1
+      });
+      console.log(this.state.currentImage, this.props.images[this.state.currentImage]);
+    } else {
+      this.setState({
+        currentImage: 0
+      });
+      console.log(this.state.currentImage,this.props.images[this.state.currentImage]);
+    }
+    this.setState({seconds: this.state.seconds + 1});
+    if(this.state.seconds > 7){
+        clearInterval(this.timer);
+    }
+    return this.currentImage;
+  }
+
+  componentDidMount() {
+        this.timer = setInterval(this.switchImage, 1000);     
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  }
+
+  handleImageLoaded() {
+    this.setState({ imageStatus: "" });
+  }
+
+  render() {
+    return (
+      <div className="image_style">
+        <img
+          src={this.props.images[this.state.currentImage]}
+          alt="Loading image..."
+        />
+      </div>
+    );
+  }
+}
+export default ShowImage;
